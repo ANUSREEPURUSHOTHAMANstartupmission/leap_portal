da010b422805b745a5cbd3baba3d2578d0a542d8
Anusree Purushothaman <anusreepurushothaman@Anusrees-MacBook-Pro.local>
2025-01-08 11:41:51 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
+  base:'/leap_portal',
+  integrations: [tailwind()]
 });
